3a6fc7dd7ee976e9e3a7b1df269773ba3bf2e357
Craig Vyvial <cp16net@gmail.com>
2014-06-20 02:14:27 -0500

    removing python.tar (its too big for GH) make the examples.sh script download the python.tar

--- examples.sh
+++ examples.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+cd ~
+wget http://packages.zerovm.org/zerovm-samples/python.tar
 
 echo 'print "Hello"' > hello.py
-zvsh --zvm-image /vagrant/python.tar python @hello.py
+zvsh --zvm-image ~/python.tar python @hello.py
 
 cd /vagrant/zerovm-samples
 make
